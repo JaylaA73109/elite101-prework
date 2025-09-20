e47bdfbb75e057c9244209fbfddc6cff61274bf3
JaylaA73109 <Jaylaarvie09@gmail.com>
2025-09-19 20:13:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-print("Welcome to the retail store chatbox")
+print("Welcome to the Elite101 Chatbot!")
 name = input("What is your name?")
-age = input("Hello " + name + ", how old are you?") 
-print("Bye!")
+age = input("What is your age?")
+assist = input("Hello " + name + ", how can I assist you?\n *Type menu to see the options") 
+def display_menu():
+    print("\n**Menu**")
+    print("1.Browse Products")
+    print("2.Check Order Status")
+    print("3.Deals and Promotions")
+    print("4.Store Information")
+    print("5.Customer Support")
+    print("6.Account Help")
+    print("7.Exit")
+display_menu()
+user_response = input(f'Enter a number between 1-7')
+
+if user_response == "7":
+    print("Okay then, Goodbye!")
+
 
